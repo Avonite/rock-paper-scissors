578fe91649bee532125c7a60403fcdf9a2c35ced
remco <remcodenheijer@gmail.com>
2018-05-22 10:53:05 +0200

    add buttons reset button needs work

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 let playerScore = 0;
 let playerPCScore = 0;
+let playButtons = document.querySelectorAll('.playButtons');
+let resetButton = document.querySelector('#resetButton');
 
-game();
+addClickEventsPlayButtons();
 
-function game() {
-	for(i = 0; i < 5; i++) {
-		let player = prompt("Choose rock, paper or scissors.").toLowerCase();
-		while (!checkUserInput(player)) {
-			alert("That was incorrect.");
-			player = prompt("Choose rock, paper or scissors.").toLowerCase();
-		}
-		let playerPC = computerPlayer();
-		console.log(getWinner(player, playerPC));
-		console.log("Score: You: " + playerScore + " - Computer: " + playerPCScore);
-	}
+resetButton.addEventListener('click', function() {
+	playerScore = 0;
+	playerPCScore = 0;
+	displayScores();
+});
+
+
+function addClickEventsPlayButtons() {
+	for (let i = 0; playButtons.length; i++) {
+		playButtons[i].addEventListener('click', function() {
+			getWinner(playButtons[i].innerHTML, computerPlayer());
+			displayScores();
+		});
+	};
+}
+
+function displayScores() {
+	document.getElementById('playerScoreDisplay').textContent = playerScore;
+	document.getElementById('computerScoreDisplay').textContent = playerPCScore;
 }
 
 function computerPlayer() {
@@ -55,13 +65,34 @@ function getWinner(player, playerPC) {
 }
 
 
-function checkUserInput(player){
-	switch (player) {
-		case "rock" :
-		case "paper" :
-		case "scissors":
-			return true;
-		default: 
-			return false;
-	}
-}
+// function checkUserInput(player){
+// 	switch (player) {
+// 		case "rock" :
+// 		case "paper" :
+// 		case "scissors":
+// 			return true;
+// 		default: 
+// 			return false;
+// 	}
+// }
+
+
+// function playRound(e) {
+// 	let scoreDisplay = document.querySelector('#playerScore');
+// 	scoreDisplay.textContent("test");
+// }
+
+// game();
+
+// function game() {
+// 	for(i = 0; i < 5; i++) {
+// 		let player = prompt("Choose rock, paper or scissors.").toLowerCase();
+// 		while (!checkUserInput(player)) {
+// 			alert("That was incorrect.");
+// 			player = prompt("Choose rock, paper or scissors.").toLowerCase();
+// 		}
+// 		let playerPC = computerPlayer();
+// 		console.log(getWinner(player, playerPC));
+// 		console.log("Score: You: " + playerScore + " - Computer: " + playerPCScore);
+// 	}
+// }
